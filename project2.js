8d7545c6cedace82678376b5ded0c5aa883d6ef5
IrinaBedunkevits <55229403+IrinaBedunkevits@users.noreply.github.com>
2020-11-12 17:33:42 +0200

    Edited

--- project2.js
+++ project2.js
@@ -1,23 +1,12 @@
 $(document).ready(function(){
-    $("#getWeather").click(function(){
-        var city = $('#city').val();
-        var key = 'f9b55ed47ec85cc75b973ea3d76dd0c4';
-
-        $.ajax({
-            url: 'http://api.openweathermap.org/data/2.5/weather',
-            dataType: 'json',
-            type: 'GET',
-            data: {q:city, appid: key, units: 'metric'},
-
-            success: function(data){
-                var wf = '';
-                $.each(data.weather, function(index, val){
-                    wf += '<p><b>' + data.name + "</b><img src=" + val.icon + ".png></p>"+
-                    Math.round(data.main.temp) + '&deg;C ' + ' | ' + val.main + ", " + 
-                    val.description
-                });
-                $("#showWeather").html(wf);
-            }
-        })
-    })
-})
+    $.get(
+        "http://api.openweathermap.org/data/2.5/weather",
+        {
+            "id" : "491422",
+            "appid" : "f9b55ed47ec85cc75b973ea3d76dd0c4"
+        },
+        function(data){
+            console.log(data);
+        }
+    );
+});
