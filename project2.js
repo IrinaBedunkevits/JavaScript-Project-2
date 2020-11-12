f5a411c45fd40b55acd43a37126a8978402dcdc4
IrinaBedunkevits <55229403+IrinaBedunkevits@users.noreply.github.com>
2020-11-12 14:29:05 +0200

    Edited

--- project2.js
+++ project2.js
@@ -5,7 +5,9 @@ var desc = document.querySelector('.desc');
 var clouds = document.querySelector('.clouds');
 var button= document.querySelector('.submit');
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function()
+
+{
     fetch('http://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&units=metric&appid=f9b55ed47ec85cc75b973ea3d76dd0c4')
     .then(responce => responce.json())
     .then(data => {
