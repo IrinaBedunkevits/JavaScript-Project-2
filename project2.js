9e6bce79f52f3127a8566eb2f95a26a1b0c18790
IrinaBedunkevits <55229403+IrinaBedunkevits@users.noreply.github.com>
2020-11-12 17:41:37 +0200

    Edited

--- project2.js
+++ project2.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $.get(
         "http://api.openweathermap.org/data/2.5/weather",
         {
-            "id" : "491422",
+            "id" : "569143",
             "appid" : "f9b55ed47ec85cc75b973ea3d76dd0c4"
         },
         function(data){
