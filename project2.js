04e1b8b6d73badd2fe32ef598ceaec7e7b14800a
IrinaBedunkevits <55229403+IrinaBedunkevits@users.noreply.github.com>
2020-11-12 20:27:15 +0200

    Edited

---
+++ project2.js
@@ -0,0 +1,27 @@
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
+
+button.addEventListener('click', function()
+
+{
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&units=metric&appid=f9b55ed47ec85cc75b973ea3d76dd0c4')
+    .then(responce => responce.json())
+    .then(data => {
+        
+  var tempValue = Math.round(data['main']['temp']);
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = descValue;
+  temp.innerHTML = tempValue;
+  input.value ="";
+
+})
+
+.catch(err => alert("Can´t find city"))
+})
